34e2b003da350d6447cb03ae6688b47e10dad993
Tureen64 <jlflesher@yahoo.com>
2026-05-28 02:11:18 +0100

    Site v2.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,12 @@
-function loadBlog() {
-    fetch("docs/blogs/posts.json")
-        .then(res => res.json())
-        .then(posts => {
+document.addEventListener("DOMContentLoaded", () => {
+    const nav = document.createElement("div");
+    nav.id = "nav";
+    nav.innerHTML = `
+        <a href="/index.html">Home</a>
+        <a href="/subpage/projects.html">Projects</a>
+        <a href="/subpage/socials.html">Socials</a>
+    `;
 
-            // Don't attempt to display
-            if (!posts || posts.length === 0) return;
-
-            // Sort by newest date parameter
-            posts.sort((a, b) => new Date(b.date) - new Date(a.date));
-            const latest = posts[0];
-            const container = document.getElementById("newestBlogContainer");
-            const newest = document.getElementById("newestBlog");
-
-            if (container && newest && latest) {
-                container.style.display = "list-item";
-                newest.href = `/${latest.slug}`;
-                newest.textContent = `Latest Post: ${latest.title}`;
-            }
-        })
-        .catch(err => console.error("Could not load posts list.", err));
-}
-
-document.addEventListener("DOMContentLoaded", loadBlog)
+    // put before everything
+    document.body.prepend(nav);
+});
