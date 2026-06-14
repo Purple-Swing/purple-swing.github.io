e068026793cb99afc0bdf33cc86bff7580907e3b
Tureen64 <jlflesher@yahoo.com>
2026-06-14 02:05:03 +0100

    Site v2.6 | Newsletters Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,43 @@
 const global_link = document.createElement("link");
 global_link.href = "/global.css";
 global_link.rel = "stylesheet";
-global_link.style = "text/css";
+global_link.type = "text/css";
 document.head.appendChild(global_link);
 
-function documentLoaded() {
+async function documentLoaded() {
+    let site_version = "NAN";
+    let navbar_enabled = true;
+    let navbar_pages;
+
+    // Get site info
+    try {
+        const res = await fetch("/site-info.json");
+        const data = await res.json();
+        site_version = data.version;
+        navbar_enabled = data.navbar;
+        navbar_pages = data.navbar_pages;
+    }
+    catch (err) {
+        console.error("Couldn't get site info.", err);
+    }
+
     const nav = document.createElement("div");
-    nav.id = "nav";
-    nav.innerHTML = `
-        <a href="/index.html"><img src="/site-assets/LogoLQ.webp" style="height: 5em; padding: 0.1em 0.1em 0.1em 0.1em"></a></img>
-        <a href="/subpage/projects.html">Projects</a>
-        <a href="/subpage/socials.html">Socials</a>
-        <a href="/subpage/newsletters.html">Newsletters</a>
-        <a href="/subpage/site-updates.html">Site Updates</a>
-    `;
+    if (navbar_enabled) {
+        nav.id = "nav";
+        nav.innerHTML = `
+        <a href="/index.html"><img src="/site-assets/LogoLQ.webp" style="height: 5em; padding: 0.1em 0.1em 0.1em 0.1em"></a>`;
+
+        navbar_pages.forEach(page => {
+            nav.innerHTML += page;
+        });
+    }
 
     const copyright = document.createElement("div");
     copyright.id = "copyright";
-    copyright.innerHTML = `
-    © PURPLE SWING, 2026 | UK UNREGISTERED JOINT AUTHORSHIP | Site v2.5
+
+    // Automatically update info 
+    copyright.textContent = `
+    © PURPLE SWING, 2026 | UK UNREGISTERED JOINT AUTHORSHIP | Site v${site_version}
     `
 
     // Put before everything
--- scripts/newletter-page.js
+++ scripts/newletter-page.js
@@ -1,16 +1,21 @@
 const newsletter_link = document.createElement("link");
 newsletter_link.href = "/inner-css/newsletter.css";
 newsletter_link.rel = "stylesheet";
-newsletter_link.style = "text/css";
+newsletter_link.type = "text/css";
 document.head.appendChild(newsletter_link);
 
-document.addEventListener("DOMContentLoaded", () => { 
+document.addEventListener("DOMContentLoaded", () => {
     fetch("/newsletters.json").then(res => res.json()).then(letters => {
         const slug = window.location.pathname.match(/([^/]+)\.html$/)?.[1];
         const newsletter = letters.find(letter => letter.slug === slug);
 
-        document.title = `Purple Swing | Newsletters | ${newsletter.title}`;
+        if (newsletter.show == false) {
+            const container = document.getElementById("container");
+            container.innerText = "Selected newsletter could not be accessed and may be private.";
+            container.style = "margin: 3rem; text-align: center;";
+        }
 
+        document.title = `Purple Swing | Newsletters | ${newsletter.title}`;
         document.getElementById("head").innerHTML = `${newsletter.title} | ${newsletter.release} <span style="font-size: medium;">by ${newsletter.authors}</span>`;
     });
 });
